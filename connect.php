d91ebc450422163e5645321186c47ee229b6f69d
Alookaparatha <alookaparatha128@gmail.com>
2025-08-16 22:19:29 +0530

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,33 @@
+       <?php
+
+       $host = "sql303.byetcluster.com";
+       $user = "ezyro_39718623";
+       $pass = "012f9a8f90";
+       $db = "ezyro_39718623_login"
+
+        $con = mysqli_connect($host , $user, $pass, $db );
+
+        if (!$con) {
+            die("Connection Failed: " . mysqli_connect_error());
+        }
+        if (isset($_POST['submit'])) {
+
+            $name = $_POST['name'];
+            $age = $_POST['age'];
+            $gender = $_POST['gender'];
+            $email = $_POST['email'];
+            $phone = $_POST['phone'];
+            $other = $_POST['other'];
+
+            $query = "INSERT INTO mydata (name, age, gender, email, phone, other) values('$name','$age','$gender','$email','$phone','$other' )";
+
+            $execute = mysqli_query($con, $query);
+
+            header("Location: index.html");
+        
+}
+
+        mysqli_close($con);
+
+
+        ?>
